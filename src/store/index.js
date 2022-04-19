d947799eff87699ac131cc5519e0c255f6ed6efb
Jin0811 <15797954826@163.com>
2022-04-19 20:56:33 +0800

    添加store

--- src/store/index.js
+++ src/store/index.js
@@ -4,8 +4,20 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
+  state: {
+    cacheViews: [], // 被缓存的路由，存放的是每一个路由的name，所以name不能重复
+  },
+  mutations: {
+    ADD_CACHEVIEW(state, view) {
+      if (!state.cacheViews.includes(view.name)) {
+        state.cacheViews.push(view.name);
+      }
+    },
+    // 删除缓存的路由
+    DELETE_CACHEVIEW(state, view) {
+      state.cacheViews = state.cacheViews.filter((item) => item !== view.name);
+    },
+  },
   actions: {},
   modules: {},
 });
