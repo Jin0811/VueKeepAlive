57d57dba58a662cd52a0c3a28a6d8818477f260a
Jin0811 <15797954826@163.com>
2022-04-19 20:50:56 +0800

    添加mock数据，传递参数到详情页面

--- src/views/personManage/detail.vue
+++ src/views/personManage/detail.vue
@@ -1,13 +1,9 @@
 <template>
   <div>
     <el-descriptions title="用户信息" border>
-      <el-descriptions-item label="用户名">kooriookami</el-descriptions-item>
-      <el-descriptions-item label="手机号">18100000000</el-descriptions-item>
-      <el-descriptions-item label="居住地">苏州市</el-descriptions-item>
-      <el-descriptions-item label="备注">
-        <el-tag size="small">学校</el-tag>
-      </el-descriptions-item>
-      <el-descriptions-item label="联系地址">江苏省苏州市</el-descriptions-item>
+      <el-descriptions-item label="用户名">{{ record.name }}</el-descriptions-item>
+      <el-descriptions-item label="手机号">{{ record.phone }}</el-descriptions-item>
+      <el-descriptions-item label="居住地">{{ record.address }}</el-descriptions-item>
     </el-descriptions>
 
     <el-button
@@ -23,8 +19,10 @@
 <script>
 export default {
   name: "Detail",
-  created() {
-    console.log(this.$route);
+  data() {
+    return {
+      record: this.$route.query
+    }
   },
   methods: {
     goBack() {
--- src/views/personManage/list.vue
+++ src/views/personManage/list.vue
@@ -31,6 +31,18 @@
         </template>
       </el-table-column>
     </el-table>
+
+    <div class="pagination">
+      <el-pagination
+        @current-change="fetch"
+        :current-page.sync="page.current"
+        :page-size="page.pageSize"
+        layout="total, prev, pager, next"
+        :total="page.total"
+        background
+      >
+      </el-pagination>
+    </div>
   </div>
 </template>
 
@@ -40,16 +52,58 @@ export default {
   data() {
     return {
       tableData: [],
+      page: {
+        current: 1,
+        pageSize: 10,
+        total: 0,
+      },
     };
   },
-  created() {},
+  created() {
+    this.fetch();
+  },
   methods: {
+    // 查询表格数据（Mock数据）
+    fetch() {
+      let result = this.generateTableData(
+        this.page.current,
+        this.page.pageSize
+      );
+      this.tableData = result.map((item) => {
+        return {
+          id: item,
+          name: `姓名${item}`,
+          phone: `${item}`.repeat(5),
+          address: `地址${item}`,
+        };
+      });
+      this.page.total = 50;
+    },
+    // 生成表格数据
+    generateTableData(current, pageSize) {
+      let arr = [];
+      let begin = pageSize * (current - 1) + 1;
+      let end = pageSize * current + 1;
+      for (let i = begin; i < end; i++) {
+        arr.push(i);
+      }
+      return arr;
+    },
+    // 跳转详情页面
     toDetail(record) {
       this.$router.push({
         path: "/layout/personManage/detail",
-        params: record,
+        query: record,
       });
     },
   },
 };
 </script>
+
+<style lang="scss">
+  .pagination{
+    display: flex;
+    justify-content: flex-end;
+    margin-top: 20px;
+  }
+</style>
