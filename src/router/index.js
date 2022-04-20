87cca407b6e435302ce47b248a223b42e5fbfb04
Jin0811 <15797954826@163.com>
2022-04-20 20:52:32 +0800

    结合stroe的actions，使用async和await来确保数据存储完毕

--- src/router/index.js
+++ src/router/index.js
@@ -9,28 +9,26 @@ const router = new VueRouter({
   routes,
 });
 
-router.beforeEach((to, from, next) => {
+router.beforeEach(async (to, from, next) => {
   // 如果to需要进行缓存，则直接进行缓存，无论是从哪里进入，都先进行缓存
   if (to.meta && to.meta.keepAlive) {
-    Store.commit("ADD_CACHEVIEW", to);
+    await Store.dispatch("ADD_CACHEVIEW_ACTION", to);
   }
 
   // 如果to的cacheFrom存在，并且cacheFrom不包含from的name，说明是从不需要缓存to的页面进入to的，这个时候就删除to的缓存
   // 即从其他的菜单项，进入keepAlive的页面，需要删除进入页面的缓存
   if (to.meta.cacheFrom && !to.meta.cacheFrom.includes(from.name)) {
-    Store.commit("DELETE_CACHEVIEW", to);
+    await Store.dispatch("DELETE_CACHEVIEW_ACTION", to);
   }
 
   // 如果从需要缓存的页面，跳转到其他页面，则先缓存from
   // 例如：从list跳转detail，缓存list
   if (from.meta && from.meta.keepAlive) {
-    Store.commit("ADD_CACHEVIEW", from);
+    await Store.dispatch("ADD_CACHEVIEW_ACTION", from);
   }
 
-  // 这里不进行延时的话，会有一定几率，数据还没存到vuex当中，页面已经跳转完成了，就会造成页面缓存功能出现Bug
-  setTimeout(() => {
-    next();
-  }, 100);
+  // 处理完成缓存的数据之后，放行路由
+  next();
 });
 
 export default router;
--- src/store/index.js
+++ src/store/index.js
@@ -8,6 +8,7 @@ export default new Vuex.Store({
     cacheViews: [], // 被缓存的路由，存放的是每一个路由的name，所以name不能重复
   },
   mutations: {
+    // 添加缓存的路由
     ADD_CACHEVIEW(state, view) {
       if (!state.cacheViews.includes(view.name)) {
         state.cacheViews.push(view.name);
@@ -18,6 +19,15 @@ export default new Vuex.Store({
       state.cacheViews = state.cacheViews.filter((item) => item !== view.name);
     },
   },
-  actions: {},
-  modules: {},
+  actions: {
+    // 之所以使用actions，是为了可以在路由钩子当中使用 async...await 来确保数据存储完毕
+    // 添加缓存的路由
+    ADD_CACHEVIEW_ACTION(context, view) {
+      context.commit("ADD_CACHEVIEW", view);
+    },
+    // 删除缓存的路由
+    DELETE_CACHEVIEW_ACTION(context, view) {
+      context.commit("DELETE_CACHEVIEW", view);
+    },
+  },
 });
